9565a5655f047a83d958a69fb8ae4a5c15624dba
urfan03 <urfanmusayev2003@gmail.com>
2024-07-14 19:17:39 +0400

    done

--- Webpack/task_3/modules/body/body.js
+++ Webpack/task_3/modules/body/body.js
@@ -1,22 +1,4 @@
-const $ = require("jquery");
-const _ = require("lodash");
-import "./body.css";
+import './body.css';
+import $ from 'jquery';
+import _ from 'lodash';
 
-let count = 0;
-
-function updateCounter() {
-  count++;
-  $("#count").text(`${count} clicks on the button`);
-}
-
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-  })
-);
-
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,54 +1,46 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
+	entry: {
+		all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
+	  },  output: {
     path: path.resolve(__dirname, 'public'),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
-  performance: {
-    hints: false,
-    maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-  },
-  devServer: {
-    contentBase: path.join(__dirname, './public'),
-    compress: true,
-    port: 8564,
-  },
+	filename: '[name].bundle.js',
+},
+plugins: [
+	new CleanWebpackPlugin(),
+	new HtmlWebpackPlugin({
+	title: 'Holberton Dashboard',
+    }),
+	],
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './public',
+		port: 8564,
+	},
+
+  mode: 'development',
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-    ],
-  },
+	rules: [
+		{
+			test: /\.css$/i,
+			use: ["css-loader", "style-loader"],
+		},
+		{
+			test: /\.(gif|png|jpe?g|svg)$/i,
+			use: [
+				"file-loader",
+				{
+					loader: "image-webpack-loader",
+					options: {
+							bypassingOnDebug: true,
+							disable: true,
+					},
+				},
+			],
+		},
+	],
+},
 };
